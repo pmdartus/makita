b6f10d135b8c24add025bcd4d3c08352a9c81698
Pierre-Marie Dartus <p.dartus@salesforce.com>
2023-03-03 08:12:23 +0100

    Baseline

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const path = require('node:path');
+
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
+app.use(morgan('tiny'));
+
 app.use(express.static('public'));
-app.use(express.static('routes'));
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'routes/contact.html'));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
